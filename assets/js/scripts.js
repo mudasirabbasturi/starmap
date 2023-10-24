cb823af119986d6d9d735bd3cf883045766eac0c
Mudasir Abbas Turi <44305478+mudasirabbasturi@users.noreply.github.com>
2023-10-24 19:02:48 +0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -515,7 +515,7 @@ starDensitySizeEle.addEventListener('input', function(e) {
 
 function convertToImgDownload() {
     document.getElementById('loader').style.visibility = "visible";
-    const dpi = 3000;
+    const dpi = 1500;
     const scale = dpi / 96;
     setTimeout(function() {
       html2canvas(frameEle, {
